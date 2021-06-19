bf3bc38eb1ba5ac76298d4bdef9f29d062c74c17
Ein_Green <mbananola2002@gmail.com>
2021-06-19 14:17:46 -0600

    created an app.py file and imported all necessary files. Added if functions for running the file for debug purposes

---
+++ app.py
@@ -0,0 +1,28 @@
+import traceback
+import dbconnect
+from flask import Flask, request, Response
+import json
+import mariadb
+import secrets
+import sys
+
+app = Flask(__name__)
+
+
+
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print("No mode argument, please pass a mode argument when invoking the file")
+    exit()
+
+if(mode == "production"):
+    import bjoern
+    bjoern.run(app, "0.0.0.0", 5021)
+elif(mode == "testing"):
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+else:
+    print("Invalid mode, please select either 'production' or 'testing'")
+    exit()
