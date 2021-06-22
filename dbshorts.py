5e3c144b7197015d44ff98dddc10f3a12881c163
Ein_Green <mbananola2002@gmail.com>
2021-06-22 15:02:44 -0600

    created functions for select, insert, delete, and update sql statement commands. Found even more spelling errors

--- dbshorts.py
+++ dbshorts.py
@@ -1,6 +1,23 @@
 import dbconnect
 import traceback
 
+# For GET
+def run_selection(statement, params):
+    conn = dbconnect.get_db_connection()
+    cursor = dbconnect.get_db_cursor(conn)
+    result = None
+
+    try:
+        cursor.execute(statement, params)
+        result = cursor.fetchall()
+    except:
+        traceback.print_exc()
+        print("Some error has occured, I don't freakin know dude")
+    dbconnect.close_db_cursor(cursor)
+    dbconnect.close_db_connection(conn)
+    return result
+
+# For POST
 def run_insertion(statement, params):
     conn = dbconnect.get_db_connection()
     cursor = dbconnect.get_db_cursor(conn)
@@ -13,6 +30,42 @@ def run_insertion(statement, params):
     except:
         traceback.print_exc()
         print("Unknown Error has occured")
+
+    dbconnect.close_db_cursor(cursor)
+    dbconnect.close_db_connection(conn)
+    return result
+
+# For DELETE
+def run_deletion(statement, params):
+    conn = dbconnect.get_db_connection()
+    cursor = dbconnect.get_db_cursor(conn)
+    result = None
+
+    try:
+        cursor.execute(statement, params)
+        conn.commit()
+        result = cursor.rowcount
+    except:
+        traceback.print_exc()
+        print("Nani? Bakana, I can't delete?")
+
+    dbconnect.close_db_cursor(cursor)
+    dbconnect.close_db_connection(conn)
+    return result
+
+# For PATCH
+def run_update(statement, params):
+    conn = dbconnect.get_db_connection()
+    cursor = dbconnect.get_db_cursor(conn)
+    result = None
+
+    try:
+        cursor.execute(statement, params)
+        conn.commit()
+        result = cursor.rowcount
+    except:
+        traceback.print_exc()
+        print("Uh oh, I can't update for some reason")
 
     dbconnect.close_db_cursor(cursor)
     dbconnect.close_db_connection(conn)
